0bace4d96d8a54730e7c31ddb8eba997a3b8d14c
Helmi Bejaoui <helmi.bejaoui@esprit.tn>
2021-03-29 15:18:08 +0200

    inject entitymanager interface

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +15,13 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class AuthController extends ApiController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/api/register", name="register", methods={"POST"})
      * @param Request $request
@@ -22,7 +30,6 @@ class AuthController extends ApiController
      */
     public function register(Request $request, UserPasswordEncoderInterface $encoder): JsonResponse
     {
-        $em = $this->getDoctrine()->getManager();
         $request = $this->transformJsonBody($request);
         $username = $request->get('username');
         $password = $request->get('password');
@@ -37,8 +44,8 @@ class AuthController extends ApiController
         $user->setPassword($encoder->encodePassword($user, $password));
         $user->setEmail($email);
         $user->setUsername($username);
-        $em->persist($user);
-        $em->flush();
+        $this->em->persist($user);
+        $this->em > flush();
         return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
     }
 
